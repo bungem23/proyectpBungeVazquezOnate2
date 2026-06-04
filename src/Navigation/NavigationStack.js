f23243490749b81380fbc071615b5e793482c0fa
mpvazquez <mpvazquez@gmail.com>
2026-06-04 17:51:34 -0300

    avances en navegacion

--- src/Navigation/NavigationStack.js
+++ src/Navigation/NavigationStack.js
@@ -1,1 +1,32 @@
-hola
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from '../Screen/Login'
+import Register from '../Screen/Register'
+import NavigationTabs from './NavigationTabs'
+
+
+
+
+import { FA6Style } from "@expo/vector-icons/build/FontAwesome6";
+
+
+const Stack=createNativeStackNavigator();
+
+
+function Navegacionstack(props){
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                
+                <Stack.Screen name="Register" component={Register} options={{headerShown: false}}/>
+                <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+                <Stack.Screen name="NavigationTabs" component={NavigationTabs} options={{headerShown: false}}/>
+           
+               
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+
+export default Navegacionstack
--- src/Screen/Register.js
+++ src/Screen/Register.js
@@ -1,1 +1,15 @@
-hola
+import React from 'react';
+import { useState } from 'react';
+import { Text, View, StyleSheet, Pressable, TextInput } from 'react-native';
+
+function Register(props) {
+    return( 
+        <View>
+            <Text>Register</Text>
+              <Pressable onPress={() => props.navigation.navigate('Login')}>
+                <Text>Ir a Login</Text>
+            </Pressable>
+        </View>
+    )
+}
+export default Register
