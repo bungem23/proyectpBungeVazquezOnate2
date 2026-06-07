ce01a3476034113c5695adba49bc5d1f602960e7
aonate@udesa.edu.ar <aonate@udesa.edu.ar>
2026-06-07 18:08:09 -0300

    x

--- src/Navigation/NavigationStack.js
+++ src/Navigation/NavigationStack.js
@@ -1,1 +1,22 @@
-hola
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from '../Screens/Login';
+import Register from '../Screens/Register';
+import Home from '../Screens/Home';
+import NavigationTabs from './NavigationTabs';
+
+const Stack = createNativeStackNavigator();
+
+function NavigationStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="NavigationTabs" component={NavigationTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default NavigationStack;
