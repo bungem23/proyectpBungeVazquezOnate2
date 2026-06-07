48674da88f1bb0d860e98d05f4536e01491ed8e9
mpvazquez <mpvazquez@gmail.com>
2026-06-07 12:46:09 -0300

    mini arreglo en App

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import NavegacionStackgit  from './src/Navigation/NavigationStack'
+import NavegacionStack  from './src/Navigation/NavigationStack'
 
 export default function App() {
   return (
